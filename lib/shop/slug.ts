48b34e9f2f6b47b040b44662818ea55fd3e24356
Joel Adu-Kwarteng <info@shipwithgodday.com>
2026-05-18 17:54:06 -0400

    refactor: use Unicode escapes in slug diacritic regex

--- lib/shop/slug.ts
+++ lib/shop/slug.ts
@@ -2,8 +2,8 @@ export function slugify(input: string): string {
   return input
     .toLowerCase()
     .normalize('NFKD')
-    .replace(/[̀-ͯ]/g, '')
-    .replace(/['‘’‚‛′‵]/g, '')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[‘’‚‛′‵]/g, '')
     .replace(/[^a-z0-9]+/g, '-')
     .replace(/^-+|-+$/g, '');
 }
